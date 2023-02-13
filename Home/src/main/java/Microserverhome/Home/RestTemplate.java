11b9182c24744fdd5eba546729cdab2f0b46e602
bhuvanesh <bhuvaneshsekar29101995@gmail.com>
2023-02-13 17:25:03 +0530

    springprofiles

---
+++ Home/src/main/java/Microserverhome/Home/RestTemplate.java
@@ -0,0 +1,5 @@
+package Microserverhome.Home;
+
+public class RestTemplate {
+
+}
---
+++ springProfiles/src/main/java/com/springprofiles/springProfiles/SpringProfilesApplication.java
@@ -0,0 +1,13 @@
+package com.springprofiles.springProfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProfilesApplication.class, args);
+	}
+
+}
